385cd7b714276a83419c54caf5772a049131dd9b
MakoDoli <makodolidze@hotmail.com>
2024-11-22 12:58:09 +0400

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img
+        className="w-[148px] h-[40px] text-red-500 absolute top-6 left-8 bg-gradient-to-b from-black "
+        src="./icons/logo.svg"
+        alt="logo"
+      />
+    </div>
+  );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Header from "./Header";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/4690cab8-243a-4552-baef-1fb415632f74/web/GE-en-20241118-TRIFECTA-perspective_bc767956-f12f-486f-8f83-f5b493d961cc_medium.jpg"
+          alt="bg"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
 }
